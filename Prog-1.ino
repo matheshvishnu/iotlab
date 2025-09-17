b87e459dc9bb4352890c4559e512bd88b20d4acb
matheshvishnu <164467606+matheshvishnu@users.noreply.github.com>
2025-09-17 23:34:13 +0530

    Update Prog-1.ino

--- Prog-1.ino
+++ Prog-1.ino
@@ -5,32 +5,31 @@ int LED[]={18,19,23,25,26};
 void setup() {
   for(int i=0;i<5;i++)
   {
-  pinMode(LED[i],OUTPUT);
+    pinMode(LED[i],OUTPUT);
   }
 }
 
 void loop() {
  
-   for(int i=0;i<5;i++)
+  for(int i=0;i<5;i++)
   {
-  LED_Clear();
-  digitalWrite(LED[i],HIGH); 
-  delay(1000);
+    LED_Clear();
+    digitalWrite(LED[i],HIGH); 
+    delay(1000);
   }
-   for(int i=3;i>0;i--)
+  for(int i=3;i>0;i--)
   {
-  LED_Clear();
-  digitalWrite(LED[i],HIGH); 
-  delay(1000);
+    LED_Clear();
+    digitalWrite(LED[i],HIGH); 
+    delay(1000);
   }
 }
 
- void LED_Clear()
- {
+void LED_Clear()
+{
   for(int i=0;i<5;i++)
   {
-  digitalWrite(LED[i],LOW); 
+    digitalWrite(LED[i],LOW); 
   }
-  
- }
+}
  
