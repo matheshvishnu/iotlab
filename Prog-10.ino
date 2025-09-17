741111617b7f311c5e40e456cbdf1ce82b42ec13
matheshvishnu <164467606+matheshvishnu@users.noreply.github.com>
2025-09-17 23:42:13 +0530

    Add files via upload

---
+++ Prog-10.ino
@@ -0,0 +1,13 @@
+// Develop a program to setup a UART protocol and pass a string through the protocol.
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("SST Technologies!");
+  Serial.println("CMR Institute of Technology!");
+  delay(1000);
+}
